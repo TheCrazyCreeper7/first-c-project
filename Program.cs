2ffa4c9dad61e237e3de4b7a04d2666efc8c74b8
TheCrazyCreeper7 <82992872+TheCrazyCreeper7@users.noreply.github.com>
2021-05-10 14:52:50 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace first_c__project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "c# project";
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WindowHeight = 40;
+            Console.WriteLine("hello, i am your PC. what your name?");
+            string userName = Console.ReadLine();
+            Console.WriteLine("hello " + userName + ",\nmy name is RX-9000");
+            Console.WriteLine("where are you from?");
+            string userL = Console.ReadLine();
+            Console.WriteLine("no way! " + userL + " is my favorite place");
+            Console.WriteLine("if you want you can prase enter to close me");
+            Console.ReadKey();
+        }
+    }
+}
